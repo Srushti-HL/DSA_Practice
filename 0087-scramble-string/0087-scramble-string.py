1887a00f964bcee6b1b353130cc3f93bfdc608dc
Srushti H L <meghanasrushti@gmail.com>
2026-07-24 22:26:18 +0530

    Time: 11 ms (76.57%), Space: 19.8 MB (65.69%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def isScramble(self, s1, s2):
+        memo = {}
+
+        def dfs(a, b):
+            if (a, b) in memo:
+                return memo[(a, b)]
+
+            # If both strings are equal
+            if a == b:
+                return True
+
+            # Different characters => impossible
+            if sorted(a) != sorted(b):
+                return False
+
+            n = len(a)
+
+            for i in range(1, n):
+
+                # No swap case
+                if dfs(a[:i], b[:i]) and dfs(a[i:], b[i:]):
+                    memo[(a, b)] = True
+                    return True
+
+                # Swap case
+                if dfs(a[:i], b[n-i:]) and dfs(a[i:], b[:n-i]):
+                    memo[(a, b)] = True
+                    return True
+
+            memo[(a, b)] = False
+            return False
+
+        return dfs(s1, s2)
