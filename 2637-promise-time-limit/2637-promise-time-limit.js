f4929e1271bfc602a143fff93f7859ec7580abaf
Srushti H L <meghanasrushti@gmail.com>
2026-08-19 10:27:24 +0530

    Time: 54 ms (35.83%), Space: 54.4 MB (33.3%) - LeetHub

---
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,15 @@
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+
+        const timeout = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+        });
+
+        return Promise.race([
+            fn(...args),
+            timeout
+        ]);
+    };
+};
