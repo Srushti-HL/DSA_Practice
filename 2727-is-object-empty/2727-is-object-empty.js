f8e033121fef04d4424f68d543adae0e2364f682
Srushti H L <meghanasrushti@gmail.com>
2026-08-24 14:40:31 +0530

    Time: 42 ms (63.55%), Space: 54 MB (58.18%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,11 @@
+var isEmpty = function(obj) {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    }
+
+    for (let key in obj) {
+        return false;
+    }
+
+    return true;
+};
