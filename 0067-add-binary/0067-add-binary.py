5432022d2f1615fdc7c1ec3e881e2a53b39dc249
Srushti H L <meghanasrushti@gmail.com>
2026-08-29 21:26:20 +0530

    Time: 3 ms (41.47%), Space: 19.2 MB (85.1%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,22 +1,22 @@
 class Solution:
-    def addBinary(self, a, b):
+    def addBinary(self, a: str, b: str) -> str:
         i = len(a) - 1
         j = len(b) - 1
         carry = 0
-        res = []
+        result = ""
 
         while i >= 0 or j >= 0 or carry:
             total = carry
 
             if i >= 0:
-                total += ord(a[i]) - ord('0')
+                total += int(a[i])
                 i -= 1
 
             if j >= 0:
-                total += ord(b[j]) - ord('0')
+                total += int(b[j])
                 j -= 1
 
-            res.append(str(total % 2))
+            result = str(total % 2) + result
             carry = total // 2
 
-        return ''.join(reversed(res))
+        return result
